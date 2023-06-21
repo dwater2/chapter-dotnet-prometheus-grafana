0fbe441074e1db4a7b6e0d7dcdf0a595308808d0
Donizete Waterkemper <dwater2@gmail.com>
2023-06-21 10:48:17 -0300

    Kubernetes-Parte 01

---
+++ Aplicacao/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿namespace Aplicacao.Models
+{
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+    using Newtonsoft.Json;
+
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Name")]
+        [JsonProperty("Name")]
+        public string BookName { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; } = null!;
+
+        public string Author { get; set; } = null!;
+    }
+}
--- Aplicacao/Startup.cs
+++ Aplicacao/Startup.cs
@@ -1,8 +1,11 @@
+using Aplicacao.Models.BooksApi.Models;
+using Aplicacao.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Prometheus;
 using Prometheus.DotNetRuntime;
 using System;
@@ -30,7 +33,17 @@ namespace Aplicacao
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // requires using Microsoft.Extensions.Options
+            services.Configure<BookstoreDatabaseSettings>(
+                Configuration.GetSection(nameof(BookstoreDatabaseSettings)));
+
+            services.AddSingleton<IBookstoreDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<BookstoreDatabaseSettings>>().Value);
+
+            services.AddSingleton<BookService>();
+
             services.AddRazorPages();
+            services.AddControllers().AddNewtonsoftJson(options => options.UseMemberCasing());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
